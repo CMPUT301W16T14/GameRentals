5ba33d348a15c349f1223df23fe325df5f63782c
donkeyoyo <donkeyoyo@gmail.com>
2016-03-03 11:33:09 -0700

    modify layout

---
+++ GameRentals/app/src/main/java/t14/com/GameRentals/MyItemsFragment.java
@@ -0,0 +1,23 @@
+package t14.com.GameRentals;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yourui on 3/2/16.
+ */
+public class MyItemsFragment extends Fragment {
+
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        return inflater.inflate(R.layout.my_items, container,false);
+    }
+}
---
+++ GameRentals/app/src/main/java/t14/com/GameRentals/PagerAdapter.java
@@ -0,0 +1,37 @@
+package t14.com.GameRentals;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by yourui on 3/2/16.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return new MyItemsFragment();
+            case 1:
+                return new BorrowFragment();
+            case 2:
+                return new BidsFragment();
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+}
