aee9b40869a709733c1797e207a7a9edcdf52713
austinredmond <aredmond@ualberta.ca>
2016-03-03 17:44:11 -0700

    BorrowFragment update

--- GameRentals/app/src/main/java/t14/com/GameRentals/BorrowFragment.java
+++ GameRentals/app/src/main/java/t14/com/GameRentals/BorrowFragment.java
@@ -48,6 +48,7 @@ public class BorrowFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        borrowedGames = UserController.getCurrentUser().getBorrowedItems();
         adapter = new ArrayAdapter<Game>(getActivity().getApplicationContext(),
                 R.layout.game_list, borrowedGames.getList());
         borrowedGameList.setAdapter(adapter);
