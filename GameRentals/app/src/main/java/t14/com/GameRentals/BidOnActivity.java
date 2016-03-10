8312873b6339a2acabba806753aa5dc585a4248f
donkeyoyo <donkeyoyo@gmail.com>
2016-03-10 13:36:12 -0700

    test

---
+++ GameRentals/app/src/main/java/t14/com/GameRentals/BidOnActivity.java
@@ -0,0 +1,28 @@
+package t14.com.GameRentals;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by yourui on 3/4/16.
+ */
+public class BidOnActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_result_for_bid);
+        Button bidButton = (Button)findViewById(R.id.BidButton);
+
+        bidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BidOnActivity.this, BidOnGameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
