380ef40abd9ebf4ee069c62c5af21dc7353ff24b
cjresler <resler@ualberta.ca>
2016-03-10 13:12:09 -0700

    reverted

--- GameRentals/app/src/main/java/t14/com/GameRentals/BidsFragment.java
+++ GameRentals/app/src/main/java/t14/com/GameRentals/BidsFragment.java
@@ -1,32 +1,22 @@
 package t14.com.GameRentals;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 /**
  * Created by yourui on 3/2/16.
  */
 public class BidsFragment  extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.bids, container,false);
-        ListView bids = (ListView)v.findViewById(R.id.bidListView);
-
-        bids.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getActivity(), ViewBidActivity.class);
-                startActivity(intent);
-            }
-        });
-        return v;
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        return inflater.inflate(R.layout.bids, container,false);
     }
 }
