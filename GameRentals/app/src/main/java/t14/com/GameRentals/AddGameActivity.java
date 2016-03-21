c8bb3f7ce8717d04afd051a096f6efd2a6e83fe7
cjresler <resler@ualberta.ca>
2016-03-21 15:32:17 -0600

    Update AddGameActivity.java

--- GameRentals/app/src/main/java/t14/com/GameRentals/AddGameActivity.java
+++ GameRentals/app/src/main/java/t14/com/GameRentals/AddGameActivity.java
@@ -1,58 +1,63 @@
 package t14.com.GameRentals;
 
-import android.app.ActionBar;
 import android.app.Activity;
-
-import android.content.Intent;
-import android.graphics.Color;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+/** This activity handles adding games to a user's list of games.
+ *
+ */
 
 public class AddGameActivity extends Activity {
     private EditText gameName;
     private EditText gameDescription;
-    ActionBar actionbar;
-    ViewPager viewpager;
-    PagerAdapter ft;
+    private User currentUser;
+    private Game game;
 
 
     @Override
+    /** Called when activity is created */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
-        final User currentUser = UserController.getCurrentUser();
+        currentUser = UserController.getCurrentUser();
+        game = (Game) getIntent().getSerializableExtra("test");
         gameName = (EditText)findViewById(R.id.addGameNameEditText);
         gameDescription = (EditText)findViewById(R.id.addGameDescriptionEditText);
         Button okButton = (Button)findViewById(R.id.addGameOkButton);
         Button cancelButton = (Button)findViewById(R.id.addGameCancelButton);
 
+        /** Handle OK button being clicked */
         okButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String name = gameName.getText().toString();
                 String description = gameDescription.getText().toString();
+                //Verify that game name is not left empty
                 if(name.equalsIgnoreCase("")){
                     gameName.setError("Game name cannot be empty");
                 }
+                //Verify that description is not left empty
                 else if(description.equalsIgnoreCase("")){
                     gameDescription.setError("Game description cannot be empty");
                 }
                 //else, proper input
                 else{
-                    Game temp = new Game(name, description, currentUser);
-                    currentUser.addMyGame(temp);
+                    game.setGameName(name);
+                    game.setDescription(description);
+                    currentUser.getMyGames().addGame(game);
+                    //Update user and add game to server
+                    //addTestData();
+                    updateServer();
                     finish();
                 }
             }
         });
 
+        /** Return to previous screen when cancel button is clicked */
         cancelButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -62,6 +67,32 @@ public class AddGameActivity extends Activity {
         });
     }
 
-    //Formatted screen pops up for user to fill out descriptions for the new game.
+    /** Add game to server, and update current user on server to reflect added game */
+    public void updateServer(){
+        ElasticsearchGameController.AddGameTask addGameTask = new ElasticsearchGameController.AddGameTask();
+        addGameTask.execute(game);
+
+        ElasticSearchUsersController.EditUserTask ese = new ElasticSearchUsersController.EditUserTask();
+        ese.execute(currentUser);
+    }
+/*
+    public void addTestData(){
+        User test = new User("Dude", "dude", "123");
+        Game mario = new Game("Mario", "Platform", test);
+        mario.setStatus(GameController.STATUS_BORROWED);
+        test.getMyGames().addGame(mario);
+        currentUser.getBorrowedItems().addGame(mario);
+        mario.setBorrower(currentUser);
+        ElasticsearchGameController.AddGameTask addGameTask = new ElasticsearchGameController.AddGameTask();
+        addGameTask.execute(mario);
+        ElasticSearchUsersController.EditUserTask ese = new ElasticSearchUsersController.EditUserTask();
+        ese.execute(currentUser);
+
+        ElasticSearchUsersController.AddUserTask esa = new ElasticSearchUsersController.AddUserTask();
+        esa.execute(test);
+        ElasticSearchUsersController.EditUserTask ese2 = new ElasticSearchUsersController.EditUserTask();
+        ese2.execute(test);
+
+    }*/
 
 }
